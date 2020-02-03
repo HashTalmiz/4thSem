bdd3859a99c959b44847f4cd6d60dd7b8f917d45
HashTalmiz <talmiz1234@gmail.com>
2020-02-03 23:01:05 +0530

    Added Function time calculator

--- DAA/randomGenerator.c
+++ DAA/randomGenerator.c
@@ -27,20 +27,27 @@ int* randomGenerator(int n, int ll, int ul)
 
 int main()
 {
-    int r;
+    clock_t start,end;
+    double timeTaken;
 
     // Initialize Random
     srand(time(NULL));
+
     // To gennerate random number between 0 and x then rand() % (x+1)
-    int x = 10;
-    int y = 20;
-    r = rand() % (y-x+1) + x;
-    printf("The Random Number: %d\n",r);
+
+    // int x = 10;
+    // int y = 20;
+    // int r = rand() % (y-x+1) + x;
+    // printf("The Random Number: %d\n",r);
     
     
     // The Rando Generator!
+    start=clock();
     int *b=randomGenerator(10,0,10);
+    end=clock();
+    timeTaken=((double)end-start)/CLOCKS_PER_SEC;
     for(int i=0;i<10;i++)
         printf("%d ",b[i]);
     printf("\n");
+    printf("The function took %f seconds to execute \n", timeTaken);
 }
