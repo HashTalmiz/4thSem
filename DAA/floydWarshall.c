c2bdfcdb0d527527548c523c930ac3fa1579e6f9
TalmizDAALAB <talmiz1234@gmail.com>
2020-02-25 15:29:37 +0530

    Floyd Incomplete--

--- DAA/floydWarshall.c
+++ DAA/floydWarshall.c
@@ -1,7 +1,8 @@
+// Read: https://brilliant.org/wiki/floyd-warshall-algorithm/
 #include<stdio.h>
 #include<math.h>
 int max(int,int);
-void warshall(int p[10][10],int n) 
+void floyd(int p[10][10],int n) 
 {
     int i,j,k;
     for (k=1;k<=n;k++)
@@ -9,13 +10,13 @@ void warshall(int p[10][10],int n)
         for (i=1;i<=n;i++)
         {
             for (j=1;j<=n;j++)
-                p[i][j]=max(p[i][j],p[i][k]&&p[k][j]);
+                p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
         }
     }
 }
-int max(int a,int b) 
+int min(int a,int b) 
 {
-    if(a>b)
+    if(a<b)
         return(a); 
     return(b);
 }
@@ -25,11 +26,11 @@ void main() {
     scanf("%d",&n);
     printf("\nEnter the number of edges:");
     scanf("%d",&e);
-    for (i=1;i<=e;i++) 
+    printf("\nEnter the adjacency Matrix(-1 for no path):");
+    for (i=1;i<=n;i++) 
     {
-        printf("\nEnter the end vertices of edge %d:",i);
-        scanf("%d%d",&u,&v);
-        p[u][v]=1;
+        for(j=1;j<=n;j++)
+            scanf("%d",&p[i][j]);
     }
     printf("\nMatrix of input data: \n");
     for (i=1;i<=n;i++) 
@@ -38,8 +39,8 @@ void main() {
             printf("%d\t",p[i][j]);
         printf("\n");
     }
-    warshall(p,n);
-    printf("\nTransitive closure: \n");
+    floyd(p,n);
+    printf("\nTransitive : \n");
     for (i=1;i<=n;i++) 
     {
         for (j=1;j<=n;j++)
