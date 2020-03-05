1cdbef32918977c0ff6e91132dbf3d0ba0a81ee0
Talmiz Ahmed <55018280+HashTalmiz@users.noreply.github.com>
2020-03-05 16:00:47 +0530

    Fixed Indentation

--- Microcontrollers/BCDtoBinary.s
+++ Microcontrollers/BCDtoBinary.s
@@ -1,13 +1,13 @@
 				area bcd_hex,code,readonly
 					ENTRY
-RADIX			RN			0
-LNM				RN		1
-UNM				RN 		2
-LNIBBLE		RN		3
-UNIBBLE		RN		4
-RES				RN		5
-BYTE			RN		6
-NOBYTES	RN		7
+RADIX					RN		0
+LNM					RN		1
+UNM					RN 		2
+LNIBBLE					RN		3
+UNIBBLE					RN		4
+RES					RN		5
+BYTE					RN		6
+NOBYTES					RN		7
 
 					mov RES,#0
 					mov RADIX,#16
@@ -21,7 +21,6 @@ LOOP			SUB R8,R8,#1
 					AND	LNIBBLE,R9,LNM
 					AND UNIBBLE,R9,UNM
 					LSR	UNIBBLE,#4
-					MOV RES,#0
 					MLA RES,RADIX,RES,UNIBBLE
 					MLA RES,RADIX,RES,LNIBBLE
 					SUBS NOBYTES,NOBYTES,#1
@@ -31,3 +30,5 @@ LOOP			SUB R8,R8,#1
 STOP	B	STOP
 NUMBER DCD 00000125
 					END
+					
+					;Compiled with 0 errors
